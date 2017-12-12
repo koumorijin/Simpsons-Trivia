09e3614c53600f04cd43e0e501b5fc8b978f8282
koumorijin <koumorijin@gmail.com>
2017-12-11 18:26:41 -0700

    More Changes

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	
+		let questionArray = ["In 'Lisa Goes Gaga' what does Lisa nickname herself on the school blog?", "In 'The Girl Code' what do Lisa and her programming friends call their social media prdictive AI app?",
+							 "When was 'The Simpsons' first aired on braodcast television?","As of December 2017 how many episodes have been aired?","What are the names of the two reoccuring aliens on the show?",
+							 "In 'Tree House of Horror XX' what turns the citizens of Springfield into 'munchers'?","'In the Principal and the Pauper' what is Principal Seymour Skinner's actual name?",
+							 "What is the name of the local Springfield baseball team?","How many puppies did She's the Fastest, Santa's Little Helper companion greyhound, have?",
+							 "In 'Homer at the Bat' what was the fate of Ken Griffey Jr.?"];
+		let answerArray = [["Truthteller", "Snowden", "Deepthroat", "Whistleblower"],["Trevor","Princeton","Conrad","Michael"],["May 23, 1992","January 12, 1991","December 17,1989", "June 19,1985"],
+							["726","468","626","897"],["Kung and Kudo","Larry and Lyle","Lang and Lodos","Kang and Kodos"],["Radioactive Fallout","Burger Squared","A Flu from China","Weird Science"],
+							["Billy Joel","Labzhob Turgenisten","Adam Smith","Armin Tamzarian"],["The Physcicists","The Atoms","The Isotopes","The Electrons"],["10","20","17","25"],
+							["He develops 'gigantism' after drinking too much brain and nerve tonic.","He falls into the never ending pit.","He's kicked off theam for having sideburns.","He gets arrested by the Springfiled Police, and charged with every open crime."]];
+		let correctAnswerArray = ["Truthteller","Conrad","December 17, 1989","626","Kang and Kodos","Burger Squared","Armin Tamzarian","The Isotopes","25","He develops 'gigantism' after drinking too much brain and nerve tonic."];
+		let correctAnswers = 0;
+		let wrongAnswers = 0;
+		let unanswered = 0;
+
+	$("#startButton").click(function() {	
+		$(".start").empty();
+		//console.log("I was clicked");
+		for(index=0; questionAr);
+
+
+	});
+});
